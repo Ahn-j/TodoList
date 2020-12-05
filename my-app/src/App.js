0ebfbe29c693feefc3e6a4156f6012d994d49640
Ahn-j <a106280@gmail.com>
2020-12-05 14:57:34 +0900

    123
    123

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+
+  }
+  render(){
+    return (
+      <div className = "TodoList_Layout">
+        <h1>---------TodoList---------</h1>
+          <form>
+            <input type = "text"/>
+            <Button className = "TodoList_btn" variant="contained" color="secondary">Add</Button>
+               
+          </form>
+      </div>
+    )
+  }
 }
 
 export default App;
